8f647a2cebf092d5d3dc344e37dda4192cb07d82
Defoulse <aidarkhan3500@gmail.com>
2021-10-07 15:56:45 +0600

    S4.31

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>This is Home Screen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>This is Home Screen</Text>
+      <Button 
+        title="Go to components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
